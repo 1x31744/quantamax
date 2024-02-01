7b6ae7eaaf33c5a6d9dee130b2554b9480edcb97
Koopa <koopahello@gmail.com>
2024-02-01 18:39:42 +0000

    broken dumb rendering

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use cond_utils::Between;
 use sdl2::pixels::Color;
 use std::fs::File;
 use std::io::prelude::*;
+use std::mem::Discriminant;
+use std::vec;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::render::Canvas;
@@ -17,13 +19,13 @@ const HEIGHT: u32 = 500;
 
 #[derive(PartialEq)]
 struct Sphere {
-    pub radius: u32,
-    pub center: (i32, i32, i32),
-    pub color: (u8, u8, u8),
+    pub radius: i32,
+    pub center: Vec<i32>,
+    pub color: Vec<u8>,
 
 }
 impl Sphere {
-    pub fn new(radius: u32, center: (i32, i32, i32), color: (u8, u8, u8)) -> Sphere {
+    pub fn new(radius: i32, center: Vec<i32>, color: Vec<u8>) -> Sphere {
         Sphere {
             radius: radius,
             center: center,
@@ -33,7 +35,7 @@ impl Sphere {
 }
 
 
-fn put_pixel(canvas: &mut Canvas<Window>,x: i32, y:i32, color: (u8, u8, u8)) {
+fn put_pixel(canvas: &mut Canvas<Window>,x: i32, y:i32, color: Vec<u8>) {
     let window_width_half: i32 = (WIDTH/2) as i32;
     let window_height_half: i32 = (HEIGHT/2) as i32;
     //error handling for draw position out of range
@@ -49,12 +51,9 @@ fn put_pixel(canvas: &mut Canvas<Window>,x: i32, y:i32, color: (u8, u8, u8)) {
     let canvas_x: i32 = (x + window_width_half) as i32;
     let canvas_y: i32 = (window_height_half - y) as i32;
     //draw pixel
-    canvas.set_draw_color(Color::RGB(color.0, color.1, color.2));
+    canvas.set_draw_color(Color::RGB(color[0], color[1], color[2]));
     let point = Point::new(canvas_x, canvas_y);
     canvas.draw_point(point).expect("could not draw point");
-
-    
-
 
 
 
@@ -76,10 +75,11 @@ pub fn main() {
     let mut canvas = window.into_canvas().build().unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
 
-    let camera_origin = (0,0,0);
     let window_width_half: i32 = (WIDTH/2) as i32;
     let window_height_half: i32 = (HEIGHT/2) as i32;
-    let mut sphere = Sphere::new(5, (0,0, 50), (225,0,0));
+    //let mut sphere1 = Sphere::new(100, vec![2,0, -5], vec![225,0,0]);
+    let mut sphere2 = Sphere::new(2, vec![0,0,5], vec![0,225,0]);
+    let mut spheres = vec![sphere2];
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -92,7 +92,7 @@ pub fn main() {
         }
         // The rest of the game loop goes here...
 
-        canvas.set_draw_color(Color::RGB(225, 225, 225));
+        //canvas.set_draw_color(Color::RGB(225, 225, 225));
         canvas.clear();
         /* 
         let (render_width, render_height) = canvas.output_size().unwrap();
@@ -127,57 +127,89 @@ pub fn main() {
 
         for x in -window_width_half..window_width_half {
             for y in -window_height_half..window_height_half {
-                let view = canvas_to_viewport(x, y, WIDTH as i32, HEIGHT as i32, 1);
+                let view = canvas_to_viewport(x, y, (WIDTH*5) as i32, (HEIGHT*5) as i32, 1);
                 //let debug_text = format!("x, y = {}, {} \n view = {:?} \n\n", x, y, viewport);
                 //write!(debug, "{}", debug_text);
-                let color = trace_ray(camera_origin, view, 1, 100, &mut sphere);
+                let color = trace_ray(vec![0,0,0], view, 2, 200000000, &mut spheres);
+                put_pixel(&mut canvas, x, y, color)
            }
            //write!(debug, "{}", "new line");
         }
         println!("done");
-        put_pixel(&mut canvas, 1, 1, (225,0,0));
+        //put_pixel(&mut canvas, 1, 1, vec![225, 0,0]);
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        break;
     }
+    ::std::thread::sleep(Duration::new(10, 1_000_000_000u32 / 60));
 }
 
-fn trace_ray(camera_origin: (i32, i32, i32), view: (i32, i32, i32), tmin: i32, tmax: i32, sphere: &mut Sphere) -> (u8, u8, u8) {
+fn trace_ray(camera_origin: Vec<i32>, view: Vec<i32>, tmin: i32, tmax: i32, spheres: &mut Vec<Sphere>) -> Vec<u8> {
     let mut closest_sphere: Option<&mut Sphere> = None;
-    let mut closest_t: i32 = 100000000;
-    //for sphere in spheres
-    let intersects : (i32, i32) = intersect_ray_sphere(camera_origin, view, sphere); //two intersections
-    if intersects.0.within(tmin, tmax) && intersects.0 < closest_t {
-        closest_sphere = Some(sphere)
+    let mut closest_t: i32 = 2147483647;
+    for sphere in spheres{
+        let intersects : Vec<f32> = intersect_ray_sphere(&camera_origin, &view, sphere); //two intersections
+        if intersects[0].within(tmin as f32, tmax as f32) && intersects[0] < closest_t as f32 {
+            closest_t = intersects[0] as i32;
+            closest_sphere = Some(sphere);
+        }
+        if intersects[1].within(tmin as f32, tmax as f32) && intersects[1] < closest_t as f32 {
+            closest_t = intersects[1] as i32;
+            closest_sphere = Some(sphere)
+        }
+        if closest_sphere == None {
+            return vec![225, 225, 225]
+        }
     }
-    if intersects.1.within(tmin, tmax) && intersects.1 < closest_t {
-        closest_sphere = Some(sphere)
-    }
-    if closest_sphere == None {
-        return (0,0,0)
-    }
-    return closest_sphere.unwrap().color;
+    //assert_eq!(intersects,vec![0.0, 0.0]);
+    return closest_sphere.unwrap().color.clone();
 }
 
-fn intersect_ray_sphere(camera_origin: (i32, i32, i32), view: (i32, i32, i32), sphere: &mut Sphere) -> (i32, i32) {
+fn intersect_ray_sphere(camera_origin: &Vec<i32>, view: &Vec<i32>, sphere: &mut Sphere) -> Vec<f32> {
     let radius = sphere.radius;
-    let camera_to_center = (camera_origin.0 - sphere.center.0, camera_origin.1 - sphere.center.1, camera_origin.2 - sphere.center.2);
+    //let camera_to_center = vec![camera_origin[0] - sphere.center[0], camera_origin[1] - sphere.center[1], camera_origin[2] - sphere.center[2]];
+    let camera_to_center = vec![sphere.center[0] - camera_origin[0], sphere.center[1] - camera_origin[1], sphere.center[2] - camera_origin[2]];
+    //println!("{:?}",camera_to_center);
+    //ray equation moddeled by point = t*view + camera_origin
+    //circle equation moddled by dot_product(point - center, point - center) = r^2
+    //sub ray into circle: dot_product((t*view+camera_origin) - center, (t*view+camera_origin) - center) = r^2
+    // cam_to center = camorgigin - center
+    //plug in cam to center dot_product(camera_to_center + t*view, camera +t*view) = r^2
+    //expand dot product: dot_product((cam_to_center + t*view), view) + dot_product((cam_to_center + t*view), t*view)
+    //expand again: dot(cam_to_center, cam_to_center) + dot(t*view, cam_to_center) + dot(cam_to_center, t*view) + dot(t*view, t*view)
+    //finally: t^2*dot(view,view) + t*2dot(view, am_to_center) + dot(cam_to_center, cam_to_center) - r^2 = 0
+    //DO QUADRATIC, SOLVE FOR T
+    //println!("{:?}, {:?}", camera_to_center, view);
+    let a: f32 = dot_product(&view, &view) as f32;
+    let b :f32 = 2.0 * dot_product(&view, &camera_to_center) as f32;
+    let c: f32 = (dot_product(&camera_to_center, &camera_to_center) - radius.pow(2)) as f32;
 
-
-
-    todo!();
-}
-fn dot_product(a: &(i32,i32,i32), b: &(i32,i32,i32)) {
-    let mut product = 0.0;
-    for i in 0..2{
-        product += a.i * b.i
+    let discriminant: f32 = b.powf(2.0)  - 4.0*a*c;
+    if discriminant < 0.0 {
+        return vec![34359738368.0, 34359738368.0];
     }
+    println!("{:?}", discriminant);
+    //then do quadratic formula
+    let t1: f32 = (-b + f32::sqrt(discriminant)) / (2.0*a);
+    let t2: f32 = (-b - f32::sqrt(discriminant)) / (2.0*a);
+    //print!("{:?}", t1);
+    //println!("{}", t1);
+    return vec![t1, t2];
+}
+fn dot_product(a: &Vec<i32>, b: &Vec<i32>) -> i32 {
+    let mut product: i32 = 0;
+    for i in 0..a.len(){
+        product += a[i] * b[i]
+    }
+    return product;
 }
 
-fn canvas_to_viewport(x: i32, y: i32, view_width: i32, view_height: i32, distance: i32) -> (i32, i32, i32) {
+fn canvas_to_viewport(x: i32, y: i32, view_width: i32, view_height: i32, distance: i32) -> Vec<i32> {
 
     let view_x = (x*view_width/(WIDTH as i32));
     let view_y = (y*view_height/(HEIGHT as i32));
-    let view = (view_x, view_y, distance);
+    let view = vec![view_x, view_y, distance];
+    //println!("{:?}", view);
     return view;
     
 }
